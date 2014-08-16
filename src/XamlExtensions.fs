09965b4d3f325791f79c409660661c5694bff45f
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2014-08-16 00:35:10 -0400

    almost there

--- src/XamlExtensions.fs
+++ src/XamlExtensions.fs
@@ -8,9 +8,13 @@ open Xamarin.Forms
 let internal loadFromXaml = 
     typeof<Xaml.Extensions>.GetRuntimeMethods() |> Seq.find (fun x -> x.Name = "LoadFromXaml" && not x.IsPublic)
 
+let internal getCallingAssemblyHandler = 
+    typeof<Assembly>.GetRuntimeMethod( "GetCallingAssembly", Array.empty)
+
 type VisualElement with
     member this.LoadFromXamlResource(embeddedResourse : string) =
-        use stream = Assembly.GetCallingAssembly().GetManifestResourceStream( embeddedResourse)
+        let callingAssembly = getCallingAssemblyHandler.Invoke( null, Array.empty) |> unbox<Assembly>
+        use stream = callingAssembly.GetManifestResourceStream( embeddedResourse)
         use reader = new StreamReader( stream)
         let xaml = reader.ReadToEnd()
         loadFromXaml.MakeGenericMethod(this.GetType()).Invoke(null, [| this; xaml |]) |> ignore

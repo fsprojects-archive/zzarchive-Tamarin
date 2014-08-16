b62c698e629fe033c3171926f9ee88c94493fdb2
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2014-08-16 17:24:36 -0400

    publish 0.2.0-alpha

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -2,17 +2,11 @@
 open System.Reflection
 open System.Runtime.CompilerServices
 
-[<assembly: AssemblyTitle("Tamarin")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("")>]
 [<assembly: AssemblyCopyright("Dmitry Morozov")>]
-[<assembly: AssemblyTrademark("")>]
-
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
-
-[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyTitle("Tamarin")>]
+[<assembly: AssemblyProduct("Tamarin")>]
+[<assembly: AssemblyVersion("0.2.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]

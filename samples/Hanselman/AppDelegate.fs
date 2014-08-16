85f2414d628643f905ba040657f39eaa1a948cdf
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2014-08-16 19:10:49 -0400

    Hanselman app initial check-in

---
+++ samples/Hanselman/AppDelegate.fs
@@ -0,0 +1,35 @@
+﻿namespace Hanselman
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+open Xamarin.Forms
+
+open Tamarin
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    let mutable window: UIWindow = null
+
+    override this.FinishedLaunching (app, options) =
+        Forms.Init ()
+        window <- new UIWindow (UIScreen.MainScreen.Bounds)
+
+//        let view = TabbedPageDemoView()
+//        let model = MonkeyDataModel.All
+//        view.Root.BindingContext <- model
+//        view.SetBindings model
+
+        let view = ContentPage()
+        window.RootViewController <- view.CreateViewController ()
+        window.MakeKeyAndVisible ()
+        true
+
+module Main =
+    [<EntryPoint>]
+    let main args = 
+        UIApplication.Main (args, null, "AppDelegate")
+        0
+
